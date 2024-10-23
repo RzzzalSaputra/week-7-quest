4b5815e75b6d8ec261386352adbaeb4d22e8d93a
Rizal Wahyu Saputra <rizalwahyu819@gmail.com>
2024-10-24 03:06:19 +0700

    "add/make-file"

--- app.js
+++ app.js
@@ -19,6 +19,38 @@ const app = {}
     })
 } 
 
+// Script Membuat File
+app.makeFile = () =>{
+
+    // Tanya Extensi File Yang Diinginkan
+    rl.question("Masukkan Extensi File Yang Diinginkan (txt, md) : ", (extensiFile) => {
+        const type = ["txt", "md"];
+        const ef = extensiFile.toLowerCase();
+        
+        // Cek Apakah Tipe File Yang Diinginkan Tersedia
+        if(!type.includes(ef)){
+            rl.close()
+            return console.log(`Tipe File ${ef} Tidak Didukung`);
+        }
+        
+        // Tanya Nama File Yang Diinginkan
+        rl.question(`Masukkan Nama File : `, (namaFile)=>{
+
+            // Tanya Isi File Yang Diinginkan
+            rl.question("Masukkan Isi File : ", (isiFile)=>{
+                fs.writeFile(`./unorganize_folder/${namaFile}.${ef}`, isiFile, err => {
+                    if (err) {
+                        console.error(err);
+                    } else {
+                        console.log("File Berhasil Dibuat");
+                    rl.close();
+                    }
+                });
+            })
+        })
+    })
+}
+
 // To Do : lanjutkan pembuatan logic disini 
 
 
