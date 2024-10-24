8f82763cf3f1c51ecb814fc759c517645bc115e0
Rizal Wahyu Saputra <rizalwahyu819@gmail.com>
2024-10-25 02:52:00 +0700

    "add/readFolder  add/function-extension  fix/ext-sorter"

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
-const fs = require("node:fs")
+const fs = require("node:fs");
 const readline = require('node:readline');
-const path = require('path')
+const path = require('path');
+const byte = require('bytes');
 
 const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
+    input: process.stdin,
+    output: process.stdout,
 });
 
 const app = {}
+
+// Fungsi Cek Extensi File
+function extenFile(f){
+    let namaExtensi = {"txt": "Text", "md": "MarkDown", "png": "Gambar", "jpg": "Gambar"};
+    let EXT = f.slice(1);
+    if(namaExtensi[EXT]){
+        return namaExtensi[EXT]
+    }else{
+        return EXT;
+    }
+}
 
 // contoh script pembuatan folder
  app.makeFolder = () => {
@@ -52,16 +64,23 @@ app.makeFile = () =>{
     })
 }
 
+// Untuk Sortir File Ke Folder Type Masing-Masing
 app.extSorter = () =>{
     fs.readdir('./unorganize_folder/', (err, file)=>{
         if(err){
             console.log(err)
         }else{
+
                 // Akses Setiap File Array
                 file.forEach((f)=>{
-                    // Direktori File
+                    
+                    // Cek Apakah Extensi Nya ada
                     if(path.parse(f).ext !== ``){
-                    const nameFolder = path.parse(f).ext.slice(1);
+
+                    // Mendapatkan Nama Folder
+                    const nameFolder = extenFile(path.parse(f).ext);
+
+                    // Direktori File
                     const currPath = `./unorganize_folder/${f}`
                     const newPath = `./${nameFolder}/${f}`
 
@@ -78,11 +97,15 @@ app.extSorter = () =>{
                             console.log(`File ${f} Telah Berhasil Dipindahkan Ke Folder ${nameFolder}`)
                         }
                     });
+
+                    // Jika Extensi File Tidak Ada
                     }else{
                         const currPath = `./unorganize_folder/${f}`
+
+                        // Direktori Ke Folder Unknown
                         const newPath = `./unknown/${f}`
 
-                        // Buat Folder
+                        // Buat Folder Ke Unknown
                         fs.mkdir(__dirname + `/unknown`,() => {
                             console.log(`Berhasil Buat Folder unknown`);
                         });
@@ -101,6 +124,36 @@ app.extSorter = () =>{
     })
     rl.close();
 }
+
+// Membaca Isi Folder
+app.readFolder = () => {
+    let temp = [];
+    rl.question("Masukan Nama Folder : ",(folderName) => {
+        fs.readdir(`./${folderName}/`, (err, file)=>{
+            if(err){
+                console.log(err)
+            }else{
+                for (let index = 0; index < file.length; index++) {
+                    const element = file[index];
+                    try {
+                        const stat = fs.statSync(`./${folderName}/${element}`);
+                        temp.push({
+                            nama: element,
+                            extensi: path.extname(element),
+                            jenisFile: extenFile(path.parse(file[index]).ext),
+                            tanggalDibuat: stat.birthtime.toString(),
+                            ukuranFile: byte(stat.size),
+                        })
+                    } catch (error) {
+                        console.log(error);
+                    }
+                }
+                console.log(JSON.stringify(temp, null, 2));
+            }
+        })
+        rl.close()
+    })
+} 
 // To Do : lanjutkan pembuatan logic disini 
 
 
